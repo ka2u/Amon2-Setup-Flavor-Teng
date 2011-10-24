24c005fe6177da4727857195f38b78994fb00108
Kazuhiro Shibuya <stevenlabs@gmail.com>
2011-10-24 16:59:05 +0900

    Initial import.

---
+++ lib/Amon2/Setup/Flavor/Teng.pm
@@ -0,0 +1,38 @@
+package Amon2::Setup::Flavor::Teng;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Setup::Flavor::Teng -
+
+=head1 SYNOPSIS
+
+  use Amon2::Setup::Flavor::Teng;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::Teng is
+
+=head1 AUTHOR
+
+Kazuhiro Shibuya E<lt>stevenlabs at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Shibuya
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
